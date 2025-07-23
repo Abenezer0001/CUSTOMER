b4ae54ee92b5734b3f6daf75ec392158e9154eea
Abenezer0001 <abenezer.t@achievengine.com>
2025-07-23 14:38:11 +0400

    Use environment variables in staging workflow instead of .env file

---
+++ src/debug-console.js
@@ -0,0 +1,4 @@
+console.log('Environment variables loaded:');
+console.log('VITE_API_BASE_URL:', import.meta.env.VITE_API_BASE_URL);
+console.log('VITE_AUTH_API_URL:', import.meta.env.VITE_AUTH_API_URL);
+console.log('All env vars:', import.meta.env);
